bab209c7cedab66e49fd3d27892319a816f93a3c
cecilieskals <eaa25cts@students.eaaa.dk>
2026-08-06 12:46:00 +0200

    update code structure css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <header className="site-header">
       <NavLink className="brand" to="/">
-        Cecilie Thykjær Skals
+        PORTFOLIO
       </NavLink>
 
       <nav className="site-nav" aria-label="Primær navigation">
